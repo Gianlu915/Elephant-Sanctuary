8f07a3191be2f41a85a25a176612e05e52e082f2
Gianluca Liccardo <gianluca_liccardo@libero.it>
2024-11-03 11:52:43 +0700

    Added components and initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutComponent } from './components/about/about.component';
+import { VolunteerComponent } from './components/volunteer/volunteer.component';
+import { DonateComponent } from './components/donate/donate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutComponent,
+    VolunteerComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,
