3c680419387feb0f38c1543505d63ff4242745fb
Gianluca Liccardo <gianluca_liccardo@libero.it>
2024-11-03 13:03:37 +0700

    routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutComponent } from './components/about/about.component';
+import { VolunteerComponent } from './components/volunteer/volunteer.component';
+import { DonateComponent } from './components/donate/donate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    
+    {path: '', component: HomepageComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'volunteer', component: VolunteerComponent},
+    {path: 'donate', component: DonateComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
