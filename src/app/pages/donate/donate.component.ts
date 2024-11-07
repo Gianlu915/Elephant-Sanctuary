a5eb0506f7cde9ed1275f8c52cc71a1ff9ac03ea
Gianluca Liccardo <gianluca_liccardo@libero.it>
2024-11-07 15:23:02 +0700

    Added synchronous regex validators for cardNumber, expiryDate, cvv, and phone fields

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -1,34 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-donate',
   templateUrl: './donate.component.html',
-  styleUrl: './donate.component.css'
+  styleUrls: ['./donate.component.css']
 })
-export class DonateComponent {
+export class DonateComponent implements OnInit {
 
-  donationForm: FormGroup;
-  showError: boolean = false;
+  creditCardForm!: FormGroup;
+  phoneForm!: FormGroup;
   showMobilePayment: boolean = false;
   showCreditCard: boolean = true;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) {}
 
-    this.donationForm = this.fb.group({
+  ngOnInit() {
+    // form credit card
+    this.creditCardForm = this.fb.group({
       name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]], // Validatore per email
-      amount: ['', [Validators.required, Validators.min(10)]]
-    })
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      amount: ['', [Validators.required]],
+      cardNumber: ['', [Validators.required, Validators.pattern(/^\d{16}$/)]],
+      expiryDate: ['', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/)]],
+      cvv: ['', [Validators.required, Validators.pattern(/^[0-9]{3,4}$/)]]
+      });
+
+    // form mobile payment
+    this.phoneForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      amount: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.pattern(/^\+?[0-9]{9,15}$/)]]
+    });
   }
+
 
   mobilePayment() {
     this.showMobilePayment = true;
     this.showCreditCard = false;
   }
 
+
   showCreditPayment() {
     this.showCreditCard = true;
     this.showMobilePayment = false;
+  }
+
+  onSubmit() {
+    // Controlla quale form è visibile e valido
+    if (this.showCreditCard && this.creditCardForm.valid) {
+      console.log('Dati per carta di credito:', this.creditCardForm.value);
+    } else if (this.showMobilePayment && this.phoneForm.valid) {
+      console.log('Dati per pagamento mobile:', this.phoneForm.value);
+    } else {
+      console.log('Il form non è valido', this.phoneForm.value);
+    }
   }
 }
